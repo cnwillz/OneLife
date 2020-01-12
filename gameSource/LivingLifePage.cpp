c14d189b8c0bb554b0284e0d61158e952332e6ea
ShameimaruAya <wzszwj2014@gmail.com>
2020-01-12 17:29:09 +0800

    Allowing receive utf8 messages

--- gameSource/LivingLifePage.cpp
+++ gameSource/LivingLifePage.cpp
@@ -346,6 +346,38 @@ typedef struct {
 
     
 
+
+
+char* U2G(const char* utf8)
+{
+    int len = MultiByteToWideChar(CP_UTF8, 0, utf8, -1, NULL, 0);
+    wchar_t* wstr = new wchar_t[len+1];
+    memset(wstr, 0, len+1);
+    MultiByteToWideChar(CP_UTF8, 0, utf8, -1, wstr, len);
+    len = WideCharToMultiByte(CP_ACP, 0, wstr, -1, NULL, 0, NULL, NULL);
+    char* str = new char[len+1];
+    memset(str, 0, len+1);
+    WideCharToMultiByte(CP_ACP, 0, wstr, -1, str, len, NULL, NULL);
+    if(wstr) delete[] wstr;
+    return str;
+}
+//GB2312到UTF-8的转换
+char* G2U(const char* gb2312)
+{
+    int len = MultiByteToWideChar(CP_ACP, 0, gb2312, -1, NULL, 0);
+    wchar_t* wstr = new wchar_t[len+1];
+    memset(wstr, 0, len+1);
+    MultiByteToWideChar(CP_ACP, 0, gb2312, -1, wstr, len);
+    len = WideCharToMultiByte(CP_UTF8, 0, wstr, -1, NULL, 0, NULL, NULL);
+    char* str = new char[len+1];
+    memset(str, 0, len+1);
+    WideCharToMultiByte(CP_UTF8, 0, wstr, -1, str, len, NULL, NULL);
+    if(wstr) delete[] wstr;
+    return str;
+}
+
+
+
 static SimpleVector<HomePos> homePosStack;
 
 static SimpleVector<HomePos> oldHomePosStack;
@@ -13315,9 +13347,11 @@ void LivingLifePage::step() {
                 // display this message
                 
                 char messageFromServer[200];
-                sscanf( message, "MS\n%199s", messageFromServer );            
+                sscanf( message, "MS\n%199s", messageFromServer );
+
+                char* transcoded = U2G(messageFromServer);            
                 
-                char *upper = stringToUpperCase( messageFromServer );
+                char *upper = stringToUpperCase( transcoded );
                 
                 char found;
 
